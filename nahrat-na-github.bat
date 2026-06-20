dcea86978e30bbac701b285939ededcf1d529171
Frantisek Konigsmark <konigsmark@seznam.cz>
2026-06-21 01:11:42 +0200

    update 21.06.2026  1:11:42,93

--- nahrat-na-github.bat
+++ nahrat-na-github.bat
@@ -1,64 +1,30 @@
 @echo off
 chcp 65001 >nul
 setlocal
-title YouTube Dabing CZ -> GitHub (auto)
-
 set "SRC=%~dp0"
 set "DEST=%USERPROFILE%\Projects\youtube-dabing-cz"
 set "REPO=https://github.com/Konigsmark/youtube-dabing-cz.git"
+set "LOG=%~dp0push-log.txt"
 
-echo ============================================================
-echo  Nahravam YouTube Dabing CZ na GitHub
-echo  Cil: %REPO%
-echo ============================================================
-echo.
+echo ==== %DATE% %TIME% ==== > "%LOG%"
+where git >nul 2>nul || (echo GIT_MISSING >> "%LOG%" & exit /b 1)
 
-where git >nul 2>nul
-if errorlevel 1 (
-  echo [CHYBA] Git neni nainstalovan: https://git-scm.com/download/win
-  echo.
-  pause
-  exit /b 1
-)
+robocopy "%SRC%." "%DEST%" /E /XD .git >> "%LOG%" 2>&1
+cd /d "%DEST%" || (echo NO_DEST >> "%LOG%" & exit /b 1)
 
-echo [1/5] Kopiruji projekt mimo OneDrive do:
-echo       %DEST%
-robocopy "%SRC%." "%DEST%" /E /XD .git >nul
-
-cd /d "%DEST%" || (echo [CHYBA] Nelze otevrit slozku & pause & exit /b 1)
-
-echo [2/5] Inicializuji repozitar...
-if not exist ".git" git init >nul
-git symbolic-ref HEAD refs/heads/main 2>nul
+if not exist ".git" git init >> "%LOG%" 2>&1
 git config user.name "Frantisek Konigsmark"
 git config user.email "konigsmark@seznam.cz"
-
-echo [3/5] Pridavam soubory a vytvarim commit...
-git add -A
-git -c commit.gpgsign=false commit -m "YouTube Dabing CZ - prvni verze (MV3, dabing pres preklad titulku)"
-if errorlevel 1 echo (commit uz mozna existuje - pokracuji)
-
-echo [4/5] Nastavuji vzdaleny repozitar...
 git remote remove origin >nul 2>nul
 git remote add origin "%REPO%"
 git branch -M main
 
-echo [5/5] Nahravam na GitHub...
-echo     Pokud vyskoci okno prohlizece "Authorize / Sign in", potvrd ho.
-git push -u origin main
+git add -A >> "%LOG%" 2>&1
+git -c commit.gpgsign=false commit -m "update %DATE% %TIME%" >> "%LOG%" 2>&1
 
-echo.
-if errorlevel 1 (
-  echo ============================================================
-  echo  PUSH SELHAL. Opis prosim cervene radky vyse zpet do chatu.
-  echo  (Caste reseni: v okne prohlizece dokoncit prihlaseni a spustit znovu.)
-  echo ============================================================
-) else (
-  echo ============================================================
-  echo  HOTOVO! Soubory jsou na: 
-  echo  https://github.com/Konigsmark/youtube-dabing-cz
-  echo ============================================================
-)
-echo.
-echo (Toto okno zustane otevrene. Zavri ho rucne krizkem.)
-pause >nul
+echo --- PUSH --- >> "%LOG%"
+git push -u origin main >> "%LOG%" 2>&1
+echo PUSH_EXIT=%ERRORLEVEL% >> "%LOG%"
+echo --- HEAD --- >> "%LOG%"
+git log -1 --oneline >> "%LOG%" 2>&1
+echo DONE >> "%LOG%"
